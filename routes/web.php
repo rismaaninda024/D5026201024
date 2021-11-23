c9ff0fa1976d941ea2783d1121c6197e5f9ade31
Risma Aninda <rismaaninda24@gmail.com>
2021-11-24 00:31:20 +0700

    Tugas CRUD

--- routes/web.php
+++ routes/web.php
@@ -25,3 +25,16 @@ Route::get('ets', 'ViewController@showEts');
 Route::get('tugasphp', 'ViewController@showTugasphp');
 Route::POST('hasilluas', 'ViewController@showHasilform');
 
+//route CRUD
+Route::get('/pegawai','PegawaiController@index');
+
+Route::get('/pegawai/tambah','PegawaiController@tambah');
+
+Route::post('/pegawai/store','PegawaiController@store');
+
+Route::get('/pegawai/edit/{id}','PegawaiController@edit');
+
+Route::post('/pegawai/update','PegawaiController@update');
+
+Route::get('/pegawai/hapus/{id}','PegawaiController@hapus');
+
