4098ed4373f182981315b4365766c4cbdb4c6ef1
Risma Aninda <rismaaninda24@gmail.com>
2021-12-01 18:41:54 +0700

    Absen

--- routes/web.php
+++ routes/web.php
@@ -50,3 +50,11 @@ Route::get('/mutasi/edit/{id}','MutasiController@edit');
 Route::post('/mutasi/update','MutasiController@update');
 
 Route::get('/mutasii/hapus/{id}','MutasiiController@hapus');
+
+Route::get('/absen','AbsenController@index');
+Route::get('/absen/tambah','AbsenController@tambah');
+Route::post('/absen/store','AbsenController@store');
+Route::get('/absen/edit/{id}','AbsenController@edit');
+Route::post('/absen/update','AbsenController@update');
+Route::get('/absen/hapus/{id}','AbsenController@hapus');
+
